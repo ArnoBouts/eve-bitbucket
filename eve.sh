a1f484ac1c35031235b14e1aa4829cd13a114022
Eve <eve@no-cloud.fr>
2017-02-26 18:13:52 +0100

    fix regexp

--- eve.sh
+++ eve.sh
@@ -4,7 +4,7 @@ SHA=$(curl -s https://api.bitbucket.org/2.0/repositories/${PLUGIN_REPOSITORY}/re
 
 cd eve
 
-grep -H -o -r "${PLUGIN_VARIABLE} [1-9a-fA-F]*$" * | while read LINE
+grep -H -o -r "${PLUGIN_VARIABLE} [0-9a-fA-F]*$" * | while read LINE
 do
         FILE=`echo $LINE | cut -d":" -f1`
         OLD=`echo $LINE | cut -d" " -f2`
